4fbcecbfd521e289568e891235e7edfdeabfb36c
sindhu-121 <sindhu.g@egradtutor.in>
2024-06-15 14:38:13 +0530

    Revert "api"

--- egradwebsite/src/apiConfig.js
+++ egradwebsite/src/apiConfig.js
@@ -1,4 +1,8 @@
 // apiConfig.js
-
+<<<<<<< HEAD
+const BASE_URL = 'http://localhost:5000';
+=======
 const BASE_URL = 'http://localhost:5001';
+>>>>>>> d95c87ec0983559c0e94c27b8b50f47aa73051c8
+ 
 export default BASE_URL;
