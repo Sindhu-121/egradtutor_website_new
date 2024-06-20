26721dbfab13143b002260be51565442456c2834
sivabhargava2024 <163115354+sivabhargava2024@users.noreply.github.com>
2024-06-20 13:07:02 +0530

    commit

--- egradwebsite/src/EgradTutorFrontEnd/EgradTutorWebsit/ExamHomePage/ExamCourse/ExamCourse.jsx
+++ egradwebsite/src/EgradTutorFrontEnd/EgradTutorWebsit/ExamHomePage/ExamCourse/ExamCourse.jsx
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import BASE_URL from "../../../../apiConfig";
-import { useParams,Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import JSONClasses from "../../../../ThemesFolder/JSONForCSS/JSONClasses.js";
+import { ThemeContext } from "../../../../ThemesFolder/ThemeContext/Context.js";
+import { FaArrowRight } from "react-icons/fa6";
+import '../../../../styles/UGHomePage/ugHomePageTheme1.css'
+import girl_img from'../../../../styles/Girl.png'
 const ExamCourse = () => {
   const { EntranceExams_Id } = useParams();
   const [portaldata, setPortalData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const themeFromContext = useContext(ThemeContext);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -30,15 +36,29 @@ const ExamCourse = () => {
   if (error) {
     return <div>{error}</div>;
   }
+
+  const themeColor = themeFromContext[0]?.current_theme;
+  const themeDetails = JSONClasses[themeColor] || [];
   return (
-    <div> 
-     <ul>
+    <div className={`exam_courses_container ${themeDetails.themeExamCoursesContainer}`}>
+      <div className={`exam_courses_sub_container ${themeDetails.themeExamCoursesSubContainer}`}>
+      <h1 className={`exam_courses_name_heading ${themeDetails.themeExamCoursesNameHeading}`}></h1>
+      <ul>
         {portaldata.map(item => (
-          <li key={item.Portale_Id}>
-            {item.Portale_Name}
-          </li>
+          <div className={`portal_names ${themeDetails.themePortalNames}`}>
+            <li key={item.Portale_Id}>
+              <h1 alt="image">{item.Portale_Name}</h1>
+              <hr/>
+              <img src={girl_img }/>
+              <div className={`exam_portal_btn ${themeDetails.themeExamPortalBtn}`}>
+              <Link>{item.button}Explore<FaArrowRight /></Link>
+              </div>
+           
+            </li>
+          </div>
         ))}
       </ul>
+      </div>
     </div>
   )
 }
