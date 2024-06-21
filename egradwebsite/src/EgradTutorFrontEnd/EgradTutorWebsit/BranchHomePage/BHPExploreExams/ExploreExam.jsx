c9b3957858470b5ea63ab9f7e8fe3d5fe4284a81
VeenaRagi <163105402+VeenaRagi@users.noreply.github.com>
2024-06-21 10:10:45 +0530

    explore exam css

--- egradwebsite/src/EgradTutorFrontEnd/EgradTutorWebsit/BranchHomePage/BHPExploreExams/ExploreExam.jsx
+++ egradwebsite/src/EgradTutorFrontEnd/EgradTutorWebsit/BranchHomePage/BHPExploreExams/ExploreExam.jsx
@@ -1,13 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import BASE_URL from "../../../../apiConfig";
 import axios from "axios";
 import ExploreExamEdit from './ExploreExamEdit'
-import { Link,useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { ThemeContext } from "../../../../ThemesFolder/ThemeContext/Context";
+import JSONClasses from "../../../../ThemesFolder/JSONForCSS/JSONClasses";
 const ExploreExam = () => {
   const { Branch_Id } = useParams();
   const [fetchedImage, setFetchedImage] = useState(null);
   const [portalesData, setPortalesData] = useState([]);
   const [branch, setBranch] = useState(null);
+  const themeFromContext = useContext(ThemeContext);
+  const refreshChannel = new BroadcastChannel("refresh_channel");
+  // Listen for messages from other pages
+  refreshChannel.onmessage = function (event) {
+    if (event.data === "refresh_page") {
+      window.location.reload(); // Reload the page
+    }
+  };
+  const themeColor = themeFromContext[0]?.current_theme;
+  console.log(themeColor, "this is the theme json classesssssss")
+  const themeDetails = JSONClasses[themeColor] || []
+  console.log(themeDetails, "mapppping from json....")
   useEffect(() => {
     fetchBranchData();
     fetchImage();
@@ -45,42 +59,42 @@ const ExploreExam = () => {
   };
   return (
     <div>
-        <ExploreExamEdit/>
-        <div>
-              <div>
-                <div>
-                  <div>
-                    {branch &&
-                      branch.EntranceExams.map((exam, index) => (
-                        <div>
-                          <ul key={index}>
-                            <li>
-                              <Link
-                                style={{ backgroundColor: "red" }}
-                                to={`/ExamHomePage/${exam.EntranceExams_Id}`}
-                              >
-                                {exam.EntranceExams_name}
-                              </Link>
-                            </li>
-
-                            {exam.Portale_Names &&
-                              exam.Portale_Names.map((portaleName, index) => (
-                                <li key={index}>{portaleName}</li>
-                              ))}
-                          </ul>
-                        </div>
-                      ))}
+      <ExploreExamEdit />
+      <div className={`${themeDetails.themeEEParentContainer}`} >
+        <div className={`${themeDetails.themeEESubContainer}`}>
+          <div className={`${themeDetails.themeEntranceExamsContainer}`}>
+            <div className={`${themeDetails.themeExploreExamContainer}`}>
+              {branch &&
+                branch.EntranceExams.map((exam, index) => (
+                  <div className={`${themeDetails.themeEEInMap}`}>
+                    <ul key={index} className={`${themeDetails.themeEEUl}`}>
+                      <li className={`${themeDetails.themeEELi}`}>
+                        <Link className={`${themeDetails.themeEELink}`}
+                          to={`/ExamHomePage/${exam.EntranceExams_Id}`}
+                        >
+                          {exam.EntranceExams_name}
+                        </Link>
+                      </li>
+                      <div className={`${themeDetails.themePortalNames}`}>
+                        {exam.Portale_Names &&
+                          exam.Portale_Names.map((portaleName, index) => (
+                              <li key={index} className={`${themeDetails.themeEEPortalLi}`}>{portaleName}</li>
+                          ))}
+                      </div>
+                    </ul>
                   </div>
-                </div>
-                <div>
-                  {fetchedImage && (
-                    <div>
-                      <img src={fetchedImage} alt="Fetched from database" />
-                    </div>
-                  )}
-                </div>
-              </div>
+                ))}
             </div>
+          </div>
+          <div>
+            {fetchedImage && (
+              <div className={`${themeDetails.themeEEBGImage}`}>
+                <img src={fetchedImage} alt="Fetched from database" />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
