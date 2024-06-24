a009d58dfb3daa0de27eb0198272e680fca13b43
A praveena <praveena.a@egradtutor.in>
2024-06-24 11:49:09 +0530

    Default theme

--- egradwebsite/src/EgradTutorFrontEnd/EgradTutorWebsit/Footer/Footer.jsx
+++ egradwebsite/src/EgradTutorFrontEnd/EgradTutorWebsit/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 import BASE_URL from "../../../apiConfig";
 import { Link, useParams } from "react-router-dom";
 import FooterEdit from "./FooterEdit";
-
+import JSONClasses from "../../../ThemesFolder/JSONForCSS/JSONClasses";
+import { ThemeContext } from "../../../ThemesFolder/ThemeContext/Context";
+import "../../../styles/Default_landingPage_styles.css";
 const Footer = () => {
   const [dataOne, setDataOne] = useState([]);
   const [dataTwo, setDataTwo] = useState([]);
@@ -14,8 +16,10 @@ const Footer = () => {
   const [showPreviousLinksData, setShowPreviousLinksData] = useState(false);
   const [isContactUsSectionVisible, setIsContactUsSectionVisible] = useState(false);
   const [isCopyRightSectionVisible, setIsCopyRightSectionVisible] = useState(false);
-const [addeGRADTutorContent, setAddeGRADTutorContent] = useState(false);
-const [addContactUsContent,setAddContactUsContent] = useState(false);
+  const [addeGRADTutorContent, setAddeGRADTutorContent] = useState(false);
+  const [addContactUsContent, setAddContactUsContent] = useState(false);
+  const themeFromContext = useContext(ThemeContext);
+
   useEffect(() => {
     axios
       .get(`${BASE_URL}/Footer/landingfooterContentDataOne`)
@@ -84,82 +88,102 @@ const [addContactUsContent,setAddContactUsContent] = useState(false);
 
     fetchFooterCopyWriteData();
   }, []);
+
+  const themeColor = themeFromContext[0]?.current_theme;
+  console.log(themeColor, "this is the theme json classesssssss")
+  const themeDetails = JSONClasses[themeColor] || []
+  console.log(themeDetails, "mapppping from json....")
+
+
   return (
-    <div>
-      <div>
+    <div className={`Footer_Main_Container ${themeDetails.themeFooterMainContainer}`}>
+      <div className={`Footer_Sub_Container ${themeDetails.themeFooterSubContainer}`}>
 
-      <button onClick={() => setAddeGRADTutorContent(!addeGRADTutorContent)}>
-          {addeGRADTutorContent ? 'Hide eGRADTtor Form' : 'Add GRADTtorData'}
-        </button>
-        {addeGRADTutorContent && <FooterEdit type = "Add eGRADTutor" />}
+        <div className={`Footer_Content_Main_Container ${themeDetails.themeFooterContentMainContainer}`}>
 
-        <button onClick={() => setFirstPopupVisible(!FirstPopupVisible)}>
-          {FirstPopupVisible ? 'Hide eGRADTtor Form' : 'EditeGRADTtorData'}
-        </button>
-        {FirstPopupVisible && <FooterEdit type="eGRADTutor" />}
+          <div className={`Footer_FirstContent__Container ${themeDetails.themeFooterFirstContentContainer}`}>
 
-        {dataOne.map((item, index) =>
-          index === 0 ? (
-            <div key={item.Content_id}>
-              <h2 className="new_landingfooter_conatinerfristpart_item">
-                {item.content}
-              </h2>
+            <div className={`Footer_eGRADTtutor__Content ${themeDetails.themeFootereGRADTtutoContent}`}>
+              <button onClick={() => setAddeGRADTutorContent(!addeGRADTutorContent)}>
+                {addeGRADTutorContent ? 'Hide eGRADTtor Form' : 'Add GRADTtorData'}
+              </button>
+              {addeGRADTutorContent && <FooterEdit type="Add eGRADTutor" />}
+
+              <button onClick={() => setFirstPopupVisible(!FirstPopupVisible)}>
+                {FirstPopupVisible ? 'Hide eGRADTtor Form' : 'EditeGRADTtorData'}
+              </button>
+              {FirstPopupVisible && <FooterEdit type="eGRADTutor" />}
+
+              {dataOne.map((item, index) =>
+                index === 0 ? (
+                  <div key={item.Content_id} className={`Footer_FirstContent ${themeDetails.themeFooterFirstContent}`}>
+                    <h2 id="Footer_Heading">
+                      {item.content}
+                    </h2>
+                  </div>
+                ) : (
+                  <p
+                    className="new_landingfooter_conatinerfristpart_item"
+                    key={item.Content_id}
+                  >
+                    {item.content}
+                  </p>
+                )
+              )}
             </div>
-          ) : (
-            <p
-              className="new_landingfooter_conatinerfristpart_item"
-              key={item.Content_id}
-            >
-              {item.content}
-            </p>
-          )
-        )}
-      </div>
-      <div>
-        <button onClick={() => setShowFooterLinksData(!showFooterLinksData)}>
-          {showFooterLinksData ? 'Hide Add Link' : 'Add Link'}
-        </button>
-        {showFooterLinksData && <FooterEdit type="Add_Footer_Links" />}
+            <div className={`Footer_Links_Content ${themeDetails.themeFooterLinksContent}`}>
+              <button onClick={() => setShowFooterLinksData(!showFooterLinksData)}>
+                {showFooterLinksData ? 'Hide Add Link' : 'Add Link'}
+              </button>
+              {showFooterLinksData && <FooterEdit type="Add_Footer_Links" />}
 
-        <button onClick={() => setShowPreviousLinksData(!showPreviousLinksData)}>
-          {showPreviousLinksData ? 'Hide Edit Links' : 'Edit Links'}
-        </button>
-        {showPreviousLinksData && <FooterEdit type="Update_Footer_Links" />}
-        {footerLinkData.map((item) => (
-          <li key={item.Link_Id}>
-            <Link to={item.Link_Routing_Data}>{item.Link_Item}</Link>
-          </li>
-        ))}
-        <div>
-        <button onClick={() => setAddContactUsContent(!addContactUsContent)} className="editbtn">
+              <button onClick={() => setShowPreviousLinksData(!showPreviousLinksData)}>
+                {showPreviousLinksData ? 'Hide Edit Links' : 'Edit Links'}
+              </button>
+              {showPreviousLinksData && <FooterEdit type="Update_Footer_Links" />}
+              <ul>
+              {footerLinkData.map((item) => (
+                
+                <li key={item.Link_Id}>
+                  <Link to={item.Link_Routing_Data}>{item.Link_Item}</Link>
+                </li>
+              ))}
+              </ul>
+            </div>
+          </div>
+          <div className={`Footer_Contact_Us_Content ${themeDetails.themeFooterContactUsContent}`}>
+            <button onClick={() => setAddContactUsContent(!addContactUsContent)} className="editbtn">
 
-{addContactUsContent ? 'Hide ContactUs' : 'Add ContactUs Data'}
-</button>
-{addContactUsContent && <FooterEdit type="Add ContactUs" />}
+              {addContactUsContent ? 'Hide ContactUs' : 'Add ContactUs Data'}
+            </button>
+            {addContactUsContent && <FooterEdit type="Add ContactUs" />}
 
-          <button onClick={() => setIsContactUsSectionVisible(!isContactUsSectionVisible)} className="editbtn">
+            <button onClick={() => setIsContactUsSectionVisible(!isContactUsSectionVisible)} className="editbtn">
 
-            {isContactUsSectionVisible ? 'Hide ContactUs' : 'Edit ContactUs Data'}
-          </button>
-          {isContactUsSectionVisible && <FooterEdit type="ContactUs" />}
-          {dataTwo.map((item, index) =>
-            index === 0 ? (
-              <div key={item.Content_id}>
-                <h2 className="new_landingfooter_conatinersecondpart_item">
+              {isContactUsSectionVisible ? 'Hide ContactUs' : 'Edit ContactUs Data'}
+            </button>
+            {isContactUsSectionVisible && <FooterEdit type="ContactUs" />}
+            {dataTwo.map((item, index) =>
+              index === 0 ? (
+                <div key={item.Content_id} className={`Footer_ContactData ${themeDetails.themeFooterContactData}`}>
+                  <h2 className="new_landingfooter_conatinersecondpart_item">
+                    {item.content_name}
+                  </h2>
+                </div>
+              ) : (
+                <p
+                  className="new_landingfooter_conatinersecondpart_item"
+                  key={item.Content_id}
+                >
                   {item.content_name}
-                </h2>
-              </div>
-            ) : (
-              <p
-                className="new_landingfooter_conatinersecondpart_item"
-                key={item.Content_id}
-              >
-                {item.content_name}
-              </p>
-            )
-          )}
+                </p>
+              )
+            )}
+          </div>
+
         </div>
-        <div>
+
+        <div className={`Footer_Copywrite_Content_Container ${themeDetails.themeFooterCopywriteContentContainer}`}>
           <button onClick={() => setIsCopyRightSectionVisible(!isCopyRightSectionVisible)} className="editbtn">
             {isCopyRightSectionVisible ? 'Hide copywrite form' : 'Edit copywrite'}
           </button>
@@ -168,6 +192,9 @@ const [addContactUsContent,setAddContactUsContent] = useState(false);
             <li key={item.Content_id}>{item.content_name}</li>
           ))}
         </div>
+
+
+
       </div>
     </div>
   )
